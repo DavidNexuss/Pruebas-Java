3e077433ea751ee763c90528672ed62202366469
DavidNexuss <dgtgar01@gmail.com>
2017-12-29 10:11:16 +0100

    Deibid 29/12/2017 10:11:16,17

--- src/com/deibid/Primo.java
+++ src/com/deibid/Primo.java
@@ -1,17 +1,28 @@
 package com.deibid;
 
+import java.util.ArrayList;
+
 public class Primo {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(isPrime(5212)); //en la mayoria de los casos solo se hacen menos de 10 iteraciones
+		System.out.println(probability(50)); //en la mayoria de los casos solo se hacen menos de 10 iteraciones
 	}
 	
+	public static float probability(int target) { // Probabilidad de que un numero al azar del 1 a target sea primo
+		
+		ArrayList<Integer> ns = new ArrayList<>();
+		for (int i = 1; i < target + 1; i++) {
+			
+			if(isPrime(i)) ns.add(i);
+		}
+		
+		return ns.size()/(float)target;
+	}
 	public static boolean isPrime(int target) {return isPrime(2, target);}
 	public static boolean isPrime(int n,int target) {
 		
-		System.out.println(n);
-		if(n == 1 || n == target) return true;
+		if(n == 1 || n == target || target == 1) return true;
 		if(target % n == 0) return false;
 		else return isPrime(n % 2 == 0 ? n + 1: n +2, target); //Nunca compruebes si target es divisble por un par
 	}
